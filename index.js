599d047eeb742a86503aa218f290fd60401e4e20
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-05-14 01:50:46 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+const superbowlWin = (objArray)=>{
+  let objFind = objArray.find((item)=>item.result === "W")
+  if(objFind != undefined){
+    return objFind.year;
+  } else {
+    return undefined;
+  }
+}
+
+superbowlWin(objArray);
